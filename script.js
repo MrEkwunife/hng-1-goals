b1cb726a529987bcaefd9a9d2aeec4de75b67b6d
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-07-03 20:04:48 +0100

    js

---
+++ script.js
@@ -0,0 +1,31 @@
+const currentDate = new Date();
+
+const currentDayOfWeek = currentDate.getDay();
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const currentHoursUTC = currentDate.getUTCHours();
+const currentMinutesUTC = currentDate.getUTCMinutes();
+const currentSecondsUTC = currentDate.getUTCSeconds();
+
+/* Cureent Day and Time */
+const currentDayName = daysOfWeek[currentDayOfWeek];
+const currentTimeUTC =
+  currentHoursUTC.toString().padStart(2, "0") +
+  ":" +
+  currentMinutesUTC.toString().padStart(2, "0") +
+  ":" +
+  currentSecondsUTC.toString().padStart(2, "0");
+
+document.querySelector(".section-header__day-time-box").innerHTML = `
+    <span class="day" data-testid="${currentDayName}">${currentDayName}, </span>
+    <span class="time" data-testid="${currentTimeUTC}">${currentTimeUTC} UTC</span>
+`;
